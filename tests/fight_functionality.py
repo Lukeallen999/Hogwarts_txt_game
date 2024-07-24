d21f980a5a6051b0e8f6bf7230dadbe423d323bf
Lukeallen999 <lukeallen999@gmail.com>
2024-07-24 18:51:44 +0100

    Created functions for the fight

--- tests/fight_functionality.py
+++ tests/fight_functionality.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class enemey:
     def __init__(self, name, health, attack) -> None:
@@ -32,49 +33,69 @@ troll = enemey(name = 'troll',
                )
           
 
-def fight(character_name,enemy_name):
-    character_name = eval('character_name')
-    enemy_name = eval('enemy_name')
 
-    if character_name == user_character:
+
+def move_list():
+    print(f'Moves:')
+    for attack in user_character.attack.keys():
+        attack_type, attack_name, attack_damage, attack_chance = user_character.attack[attack]
+        print(f'Option: {attack}')
+        print(f'Attack Type: {attack_type}')
+        print(f'Attack: {attack_name}')
+        print(f'Damage: {attack_damage}')
+        print(f'Chance: {attack_chance*100}%\n')
+
+def attack_action(attacker: str, target: str):
+
+    attacker = eval('attacker')
+    target = eval('target')
+
+    if attacker == user_character:
+        
+        if input('Do you want to see move options?(Y/N): ').upper() == 'Y':
+            move_list()
         attack_choice = int(input('Please choose an attack from 1-3: '))
-        attack_type, attack_name, attack_damage, attack_chance = character_name.attack[attack_choice]
+        attack_type, attack_name, attack_damage, attack_chance = attacker.attack[attack_choice]
 
     else:     
-        attack_type, attack_name, attack_damage, attack_chance = character_name.attack[random.choice(list(user_character.attack.keys()))]
+        attack_type, attack_name, attack_damage, attack_chance = attacker.attack[random.choice(list(user_character.attack.keys()))]
     
-    print(f'{character_name.name} uses: {attack_name} ({attack_type}) ')
+    print(f"\n---- {attacker.name}'s turn ----")
+    
+    print(f'{attacker.name} uses: {attack_name} ({attack_type}) ')
     if random.randint(1,10) <= (attack_chance * 10):
-        print(f'{character_name.name}: Hit for {attack_damage} damage')
-        enemy_name.health -= attack_damage
+        print(f'{attacker.name}: Hit for {attack_damage} damage')
+        target.health -= attack_damage
     else:
-        print(f'{character_name.name}: Miss') 
+        print(f'{attacker.name}: Miss') 
 
-
-fight_round = 0
-print(f'Moves:')
-for attack in user_character.attack.keys():
-    attack_type, attack_name, attack_damage, attack_chance = user_character.attack[attack]
-    print(f'Option: {attack}')
-    print(f'Attack Type: {attack_type}')
-    print(f'Attack: {attack_name}')
-    print(f'Damage: {attack_damage}')
-    print(f'Chance: {attack_chance*100}%\n')
-
-while troll.health > 0 and user_character.health > 0:
-    print(f'-------------------- Round:{fight_round} --------------------')
-    fight(user_character,troll)
-    fight(troll,user_character)
-
-    print('')
-    print(f'User Health: {user_character.health}')
-    print(f'troll Health: {troll.health}')
+def battle(character_name,enemy_name):
+    fight_round = 1
     
-    print('')
-    print('')
-    fight_round += 1
-print('Please pick an attack using numbers 1,2,3')
+    while character_name.health > 0 and enemy_name.health > 0:
+        print(f'-------------------- Round:{fight_round} --------------------')
+        attack_action(attacker = user_character, target = troll)
+        print('')
+        time.sleep(1)
+        attack_action(attacker = troll,target = user_character)
 
+        print('')
+        print(f'User Health: {user_character.health}')
+        print(f'troll Health: {troll.health}')
+        
+        print('')
+        print('')
+        fight_round += 1
 
-
-
+        if character_name.health <= 0:
+            print('YOU LOSE')
+            exit()
+        
+        if enemy_name.health <= 0:
+            print('You Win')
+        
+        
+        input('PRESS ENTER')
+        print('')
+        
+battle(user_character,troll)
